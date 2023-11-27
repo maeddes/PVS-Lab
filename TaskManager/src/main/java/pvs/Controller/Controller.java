4f0be11d90f2fee88620c8d9d7924b3d7782a0cc
emir1312 <tealaemir@gmail.com>
2023-11-27 14:45:40 +0100

    first approach backend

--- TaskManager/src/main/java/pvs/Controller/Controller.java
+++ TaskManager/src/main/java/pvs/Controller/Controller.java
@@ -1,5 +1,48 @@
 package pvs.Controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pvs.Controller.DTO.TaskDTO;
+import pvs.Entity.TaskEntity;
+import pvs.Service.TaskService;
+
+@RestController
+@RequestMapping("/task")
 public class Controller {
+	
+	private final TaskService taskService;
+	
+	public Controller(TaskService taskService) {
+		this.taskService=taskService;
+	}
+	
+	@PutMapping("/create")
+	public void createTask(@RequestBody TaskDTO task) {
+		taskService.createTask(task);
+	}
+	
+	@PutMapping("/update")
+	public void updateTask(@RequestBody TaskDTO task) {
+		taskService.updateTask(task);
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<TaskEntity> getTasks(){
+		return ResponseEntity.ok(this.taskService.getTasks());
+	}
+	
+	@DeleteMapping("/del")
+	public void deleteTask(@RequestBody TaskDTO task) {
+		taskService.deleteTask(task);
+	}
+	
+	
 
 }
