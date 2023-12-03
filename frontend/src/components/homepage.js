54b48c02e8244bc889a9625849d90b848e1a79bb
darait00 <darait00@hs-esslingen.de>
2023-12-03 19:05:41 +0100

    api calls

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,5 +1,6 @@
 import './homepage.css';
 import React, { useState } from "react";
+import axios from 'axios';
 
 function HomePage() {
   const [taskId, setTaskId] = useState([123]);
@@ -16,10 +17,53 @@ function HomePage() {
     </div>
   }
 
+  const testTask = {
+    id: 1,
+    name: "idk",
+    date: "2023-10-10",
+    timeslot: 1,
+    description: "test task"
+  }
+
+  //API CALLS TO BACKEND
+  async function createTask() {
+    let json = JSON.stringify(testTask);
+    await axios.put('/task/create', { json }).then((result) => {
+      console.log(result)
+    });
+  }
+  async function updateTask(){
+    let json = JSON.stringify(testTask);
+    await axios.put('/task/update',{json}).then((result) => {
+        console.log(result)
+    });
+  }
+  async function getAllTasks() {
+    await axios.get('/task/getAll').then((result) => {
+      console.log(result)
+    });
+  }
+  async function deleteTask() {
+    let json = JSON.stringify(testTask);
+    await axios.delete('/task/del',{json}).then((result) => {
+      console.log(result)
+    });
+  }
+
+
+  function showTasksOfDate(){
+    console.log(document.getElementById("calendar"));
+  }
+
+
   return (
     <div id='container'>
       <h1>Task Manager</h1>
-      <input id='calendar' type="date"></input>
+      <button onClick={getAllTasks}>getAllFromBackend</button>
+      <button onClick={createTask}>createTask</button>
+      <button onClick={updateTask}>updateTask</button>
+      <button onClick={deleteTask}>deleteTask</button>
+      <input id='calendar' type="date" onChange={showTasksOfDate}></input>
       <div id='taskHolder'>
         <Task />
         <Task />
