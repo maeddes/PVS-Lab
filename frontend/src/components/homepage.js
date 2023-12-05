c9f88ad29c6c776237a525294e452ed7761b0bee
darait00 <darait00@hs-esslingen.de>
2023-12-05 22:01:12 +0100

    Is (probably) working + new gui.

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,145 +1,161 @@
 import './homepage.css';
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 function HomePage() {
-  const [taskId, setTaskId] = useState([123]);
-  const [name, setName] = useState(['Person1']);
-  const [date, setDate] = useState(['2023-29-11']);
-  //const [timeslot, setTimeslot] = useState([]);
-  const [desc, setDesc] = useState(['very gud task.']);
+  useEffect(() => {
+    showTasksOfDate();
+  }, []);
+  const currentDate = new Date().toISOString().split('T')[0];
+
+  //not really used because popup is alway active now.
   const [TaskPopupTrigger, setTaskPopupTrigger] = useState(false);
+
+
   const [visibleTasks, setVisibleTasks] = useState([]);
-  const [taskToEdit, setTaskToEdit] = useState({ name: '', datum: undefined, timeslot: undefined, description: '' });
-  const [currentDate, setcurrentDate] = useState(new Date().toISOString().split('T')[0]);
+  const [taskToEdit, setTaskToEdit] = useState({ datum: currentDate });
 
-  const testTask = {
-    id:1,
-    name: "idk",
-    date: "2023-10-10",
-    timeslot: 1,
-    description: "test task"
-  }
 
-  //API CALLS TO BACKEND
   async function createTask(task) {
     let json = JSON.stringify(task);
     await axios.put('/task/create', json, {
       headers: { 'Content-Type': 'application/json' }
-    }).then(() => {showTasksOfDate(); });
-
+    }).then(() => { showTasksOfDate(); });
   }
   async function deleteTask(id) {
-    await axios.delete(`/task/del/${id}`).then((result) => {showTasksOfDate(); });
+    console.log('del')
+    await axios.delete(`/task/del/${id}`).then(() => { showTasksOfDate(); });
   }
-/*
-  async function updateTask() {
-    let json = JSON.stringify(testTask);
-    await axios.put('/task/update', json, {
-      headers: { 'Content-Type': 'application/json' }
-    }).then((result) => { });
-  }*/
   async function getAllTasks() {
     return await axios.get('/task/getAll', {
       headers: { 'Content-Type': 'application/json' }
     })
   }
 
-
   function showTasksOfDate() {
     const data = getAllTasks();
-    setVisibleTasks([]);
-    let l = [];
     data.then((result) => {
-      let tasks = result.data;
-      tasks.forEach(e => {
+      setVisibleTasks([]);
+      result.data.forEach(e => {
         let date = new Date(e.datum).getDate();
         let calendar = document.getElementById("calendar");
         let calendarDate = new Date(calendar.value).getDate();
         if (date == calendarDate) {
           setVisibleTasks(old => [...old, e]);
-          l.push(e);
         }
       });
     });
   }
+
   function editTask(task) {
-    deleteTask(task.id);
     setTaskToEdit(task);
-    setTaskPopupTrigger(current => !current);
+    setTaskPopupTrigger(true);
   }
 
   const VisibleTasks = () => {
     return (
-      <div>
+      <div id='taskHolder'>
         {visibleTasks.map((visibleTask) => (
           <div className="task">
-            <button onClick={() => { deleteTask(visibleTask.id); }}>X</button>
-            <button onClick={() => { editTask(visibleTask); }}>edit</button>
-            <p>{visibleTask.id}</p>
-            <p>{visibleTask.name}</p>
-            <p>{visibleTask.description}</p>
+            <div className='taskflex'>
+              <p className='TaskFieldDescription'>Name: </p>
+              <p className='TaskFieldDescription'>Description: </p>
+
+            </div>
+
+            <div className='taskflex'>
+              <p className='TaskFieldValue'>{visibleTask.name}</p>
+              <p className='TaskFieldValue'>{visibleTask.description}</p>
+            </div>
+
+            <div className='taskflex' id='editBar'>
+              <button className='taskBarBtn' onClick={() => { editTask(visibleTask); }}>&#9998;</button>
+              <button className='taskBarBtn' onClick={() => { deleteTask(visibleTask.id); }}>X</button>
+            </div>
+
           </div>
         ))}
       </div>
     );
   }
 
-  const Task = (task) => {
-    return <div className='task'>
-      <p>{task.taskId}</p>
-      <p>{task.name}</p>
-      <p>{task.description}</p>
-    </div>
-  }
-
+  //Creates Task form with values of taskToEdit.
   const CreateTaskPopup = () => {
     return (
-      <div>
-        <div>
-          <p className='createTaskP'>Name:</p>
-          <input className='createTaskInput' id='nameInput' defaultValue={taskToEdit.name}></input>
+      <div id='taskPopup'>
+        <div id='formFlex'>
+          <div className='formFlexItem'>
+            <p className='createTaskP'>Name:</p>
+            <p className='createTaskP'>Date:</p>
+            <p className='createTaskP'>Timeslot:</p>
+            <p className='createTaskP'>description:</p>
+          </div>
 
-          <p className='createTaskP'>Date:</p>
-          <input className='createTaskInput' type='date' id='dateInput' defaultValue={taskToEdit.datum}></input>
-
-          <p className='createTaskP'>Timeslot:</p>
-          <input className='createTaskInput' id='timeSlotInput' defaultValue={taskToEdit.timeslot}></input>
-
-          <p className='createTaskP'>description:</p>
-          <input className='createTaskInput' id='descriptionInput' defaultValue={taskToEdit.description}></input>
+          <div className='formFlexItem'>
+            <input className='createTaskInput' id='nameInput' defaultValue={taskToEdit.name}></input>
+            <input className='createTaskInput' type='date' id='dateInput' defaultValue={taskToEdit.datum}></input>
+            <input className='createTaskInput' id='timeSlotInput' defaultValue={taskToEdit.timeslot}></input>
+            <input className='createTaskInput' id='descriptionInput' defaultValue={taskToEdit.description}></input>
+          </div>
         </div>
 
-        <button onClick={() => {
-          let name = document.getElementById("nameInput");
-          let date = document.getElementById("dateInput");
-          let timeslot = document.getElementById("timeSlotInput");
-          let description = document.getElementById("descriptionInput");
-          createTask({ name: name.value, date: date.value, timeslot: timeslot.value, description: description.value });
-          setTaskPopupTrigger(current => !current);
-          setTaskToEdit({ name: '', datum: undefined, timeslot: undefined, description: '' });
-        }}>create Task</button>
+        <div id='flexTaskPopupButtons'>
+          <button id='closeForm' onClick={() => { setTaskToEdit({ datum: currentDate }); setTaskPopupTrigger(false); }}>Clear</button>
+          <button onClick={() => {
+            let date = document.getElementById("dateInput");
+            let name = document.getElementById("nameInput");
+            //Condition for Creating a Task.
+            if (date.value && name.value) {
+              let timeslot = document.getElementById("timeSlotInput");
+              let description = document.getElementById("descriptionInput");
+              //if there is a task we are editing. delete the task before creating the new one.
+              if (taskToEdit.name) {
+                deleteTask(taskToEdit.id);
+              }
+              createTask({ name: name.value, date: date.value, timeslot: timeslot.value, description: description.value });
+              setTaskPopupTrigger(false);
+              setTaskToEdit({ datum: currentDate });
+            }
+          }}>Create / Update Task</button>
+        </div>
       </div>
     )
   }
 
+  function changeSelDate(change) {
+    let cal = document.getElementById('calendar')
+    let date = new Date(cal.value)
+    date.setDate(date.getDate() + change);
+    cal.value = date.toISOString().split('T')[0];
+    showTasksOfDate();
+  }
+
+  /* under taskform
+  <button onClick={() => { setTaskPopupTrigger(true); setTaskToEdit({datum:currentDate}); }}>&#43;</button>
+          {TaskPopupTrigger && <CreateTaskPopup />}*/
+
   return (
-    <div id='container'>
-      <h1>Task Manager</h1>
-      <button onClick={() => { setTaskPopupTrigger(current => !current); }}>addTask</button>
-      {TaskPopupTrigger && <CreateTaskPopup />}
+    <div>
+      <h1 id='Title'>Task Manager</h1>
 
-      <button onClick={getAllTasks}>getAllFromBackend</button>
-      <button onClick={() => { createTask(testTask) }}>createTask</button>
-      <button onClick={() => { deleteTask(testTask) }}>deleteTask</button>
-      <button onClick={()=>{console.log(currentDate)}}>testerbutton</button>
-      <input id='calendar' type="date" defaultValue={currentDate}
-        onChange={showTasksOfDate}></input>
-      <div id='taskHolder'>
-        <VisibleTasks />
+      <div id='container'>
+        <div id='TaskForm'>
+          <CreateTaskPopup />
+        </div>
+
+        <div id='TaskView'>
+          <div className='defaultFlex'>
+            <button onClick={() => { changeSelDate(-1) }}>&#8592;</button>
+            <input id='calendar' type="date" defaultValue={currentDate}
+              onChange={showTasksOfDate}></input>
+            <button onClick={() => { changeSelDate(+1) }}>&#8594;</button>
+          </div>
+          <VisibleTasks />
+        </div>
       </div>
-    </div>
 
+
+    </div>
   );
 }
 export default HomePage;
